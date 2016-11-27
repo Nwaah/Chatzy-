70d6bb1a1067bf290e33ee11e28c37645d0c6919
Nwaah <dkrol1@poczta.onet.pl>
2016-11-27 23:53:26 +0100

    Added import
    Now links the other script, use chatzy_import.user.js

--- chatzy_import.user.js
+++ chatzy_import.user.js
@@ -1,4 +1,16 @@
+// ==UserScript==
+// @name        Chatzy+
+// @namespace   chatzy
+// @description Doot doot motherfucker
+// @include     http://chatzy.com
+// @include     http://chatzy.com/*
+// @include     http://us23.chatzy.com/*
+// @include     http://us23.chatzy.com/49709886073250
+// @version     1.3.31
+// @grant       none
+// ==/UserScript==
+
 var fileref=document.createElement('script');
 fileref.setAttribute("type","text/javascript");
-fileref.setAttribute("src", "https://github.com/Nwaah/Chatzy-/raw/master/chatzy.user.js");
+fileref.setAttribute("src", "https://github.com/Nwaah/Chatzy-/raw/master/chatzy.js");
 loadjscssfile("myscript.js", "js");
