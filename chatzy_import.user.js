3d287d879fc4b138bfa127ecae148a1e131e1a53
Nwaah <dkrol1@poczta.onet.pl>
2016-11-27 16:07:07 +0100

    Create chatzy_import.user.js

---
+++ chatzy_import.user.js
@@ -0,0 +1,4 @@
+var fileref=document.createElement('script');
+fileref.setAttribute("type","text/javascript");
+fileref.setAttribute("src", "https://github.com/Nwaah/Chatzy-/raw/master/chatzy.user.js");
+loadjscssfile("myscript.js", "js");
